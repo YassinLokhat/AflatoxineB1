4ce9a9b2ae0542ebd093de551466b2ce83e013fc
Yassin Lokhat <yassinlokhat@gmail.com>
2019-06-09 08:39:52 +0400

    adding the user interfaace

---
+++ AflatoxineB1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace AflatoxineB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tStd1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bValider = new System.Windows.Forms.Button();
+            this.gEchantillons = new System.Windows.Forms.GroupBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tStd3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tStd4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tStd2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tStd5 = new System.Windows.Forms.TextBox();
+            this.gEchantillons.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tStd1
+            // 
+            this.tStd1.Location = new System.Drawing.Point(83, 12);
+            this.tStd1.Name = "tStd1";
+            this.tStd1.Size = new System.Drawing.Size(100, 20);
+            this.tStd1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Standard 1 :";
+            // 
+            // bValider
+            // 
+            this.bValider.Enabled = false;
+            this.bValider.Location = new System.Drawing.Point(15, 142);
+            this.bValider.Name = "bValider";
+            this.bValider.Size = new System.Drawing.Size(168, 37);
+            this.bValider.TabIndex = 5;
+            this.bValider.Text = "Valider les échantillons standards";
+            this.bValider.UseVisualStyleBackColor = true;
+            // 
+            // gEchantillons
+            // 
+            this.gEchantillons.Controls.Add(this.panel);
+            this.gEchantillons.Location = new System.Drawing.Point(201, 12);
+            this.gEchantillons.Name = "gEchantillons";
+            this.gEchantillons.Size = new System.Drawing.Size(372, 167);
+            this.gEchantillons.TabIndex = 320;
+            this.gEchantillons.TabStop = false;
+            this.gEchantillons.Text = "Echantillons";
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.Controls.Add(this.bAdd);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Enabled = false;
+            this.panel.Location = new System.Drawing.Point(3, 16);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(366, 148);
+            this.panel.TabIndex = 45;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(50, 15);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(32, 23);
+            this.bAdd.TabIndex = 10;
+            this.bAdd.Text = "+";
+            this.bAdd.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Standard 3 :";
+            // 
+            // tStd3
+            // 
+            this.tStd3.Location = new System.Drawing.Point(83, 64);
+            this.tStd3.Name = "tStd3";
+            this.tStd3.Size = new System.Drawing.Size(100, 20);
+            this.tStd3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Standard 4 :";
+            // 
+            // tStd4
+            // 
+            this.tStd4.Location = new System.Drawing.Point(83, 90);
+            this.tStd4.Name = "tStd4";
+            this.tStd4.Size = new System.Drawing.Size(100, 20);
+            this.tStd4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Standard 2 :";
+            // 
+            // tStd2
+            // 
+            this.tStd2.Location = new System.Drawing.Point(83, 38);
+            this.tStd2.Name = "tStd2";
+            this.tStd2.Size = new System.Drawing.Size(100, 20);
+            this.tStd2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Standard 5 :";
+            // 
+            // tStd5
+            // 
+            this.tStd5.Location = new System.Drawing.Point(83, 116);
+            this.tStd5.Name = "tStd5";
+            this.tStd5.Size = new System.Drawing.Size(100, 20);
+            this.tStd5.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 188);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tStd5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tStd4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tStd2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tStd3);
+            this.Controls.Add(this.gEchantillons);
+            this.Controls.Add(this.bValider);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tStd1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Aflatoxine B1";
+            this.gEchantillons.ResumeLayout(false);
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tStd1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bValider;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tStd3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tStd4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tStd2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tStd5;
+        public System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.GroupBox gEchantillons;
+        public System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ AflatoxineB1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AflatoxineB1
+{
+    public partial class Form1 : Form
+    {
+        public double[] ppb = { 0, 1, 5, 20, 40 };
+        public double[] absorbance = new double[5];
+        public double[] B_B0 = new double[5];
+        //public double a, b;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+
+            B_B0[0] = 100;
+
+            new Echantillon(this);
+
+            bAdd.Click += bAdd_Click;
+            tStd1.TextChanged += tStd_TextChanged;
+            tStd2.TextChanged += tStd_TextChanged;
+            tStd3.TextChanged += tStd_TextChanged;
+            tStd4.TextChanged += tStd_TextChanged;
+            tStd5.TextChanged += tStd_TextChanged;
+
+            bValider.Click += bValider_Click;
+        }
+
+        void bValider_Click(object sender, EventArgs e)
+        {
+            absorbance[0] = double.Parse(tStd1.Text.Replace(".", ","));
+            absorbance[1] = double.Parse(tStd2.Text.Replace(".", ","));
+            absorbance[2] = double.Parse(tStd3.Text.Replace(".", ","));
+            absorbance[3] = double.Parse(tStd4.Text.Replace(".", ","));
+            absorbance[4] = double.Parse(tStd5.Text.Replace(".", ","));
+            
+            for (int i = 1; i < 5; i++)
+                B_B0[i] = (int)Math.Round(absorbance[i] / absorbance[0] * B_B0[0]);
+
+            CalculAB(B_B0, ppb);
+            
+            panel.Enabled = true;
+        }
+
+        private void CalculAB(double[] X, double[] Y)
+        {
+            double sX = 0, sX2 = 0, sXY = 0, sY = 0;
+            int n = X.Count();
+
+            for (int i = 1; i < n; i++)
+            {
+                sX += X[i];
+                sY += Ln(Y[i]);
+                sX2 += X[i] * X[i];
+                sXY += X[i] * Ln(Y[i]);
+            }
+
+            b = (sX * sXY - sX2 * sY) / (sX * sX - (n - 1) * sX2);
+            //a = (sY - (n - 1) * b) / sX;
+            a = (sXY - sX * b) / sX2;
+
+            b = Math.Exp(b);
+            a = Math.Exp(a);
+        }
+
+        void tStd_TextChanged(object sender, EventArgs e)
+        {
+            DisableAll();
+
+            if (AbsorbanceValide())
+                bValider.Enabled = true;
+        }
+
+        void bAdd_Click(object sender, EventArgs e)
+        {
+            new Echantillon(this);            
+        }
+
+        private void DisableAll()
+        {
+            bValider.Enabled = false;
+            panel.Enabled = false;
+            panel.Controls.Clear();
+            bAdd.Location = new System.Drawing.Point(50, 15);
+            panel.Controls.Add(bAdd);
+            Echantillon.LastNumber = 0;
+            new Echantillon(this);
+        }
+
+        private bool AbsorbanceValide()
+        {
+            double i;
+
+            if (!double.TryParse(tStd1.Text.Replace(".", ","), out i))
+                return false;
+            if (!double.TryParse(tStd2.Text.Replace(".", ","), out i))
+                return false;
+            if (!double.TryParse(tStd3.Text.Replace(".", ","), out i))
+                return false;
+            if (!double.TryParse(tStd4.Text.Replace(".", ","), out i))
+                return false;
+            if (!double.TryParse(tStd5.Text.Replace(".", ","), out i))
+                return false;
+
+            return true;
+        }
+
+        public static double Ln(double x)
+        {
+            return Math.Log(x, Math.E);
+        }
+    }
+}
